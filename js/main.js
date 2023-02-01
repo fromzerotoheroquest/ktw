c7d301a20fadd2fd7ffb4886a15af03ad5dfb31d
VitoCorleone2022 <javier.civic@gmail.com>
2023-02-01 12:10:18 +0000

    custom pointer

--- js/main.js
+++ js/main.js
@@ -1,5 +1,20 @@
 let phase1 = document.getElementById("phase1")
-const game = {
+let viewer = document.createElement("div");
+viewer.setAttribute('id', 'viewer')
+phase1.append(viewer)
 
+
+
+function moveViewer() {
+    console.log(viewer.style)
+    
+    document.addEventListener('mousemove', function (e) {
+        let cursorX = e.clientX;
+        let cursorY = e.clientY;
+        console.log('x:', cursorX, 'y:', cursorY)
+        viewer.style.left = cursorX + 'px';
+        viewer.style.top = cursorY + 'px';
+    });
 }
 
+moveViewer()
